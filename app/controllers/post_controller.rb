e1606b54b633484b6b10918b8ae69f7e0ea145a5
FHappy <Hassan.Masroor@me.com>
2017-04-27 17:30:56 -0400

    added edit and update route for posts

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,12 +1,25 @@
 class PostController < ApplicationController
-  before_action :post_params, only: [:show]
+  before_action :post_params, only: [:show, :edit, :update]
   def show
     
   end
 
+  def edit
+
+  end
+  
   def new
 
   end
+
+  def update
+    @post.update(
+      title: params["title"],
+      content: params["content"]
+    )
+    redirect_to "/posts/#{@post.id}"
+  end
+  
 
   private
     def post_params
