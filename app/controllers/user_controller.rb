905a42ff8767039522658c3c48cb7902692da594
michael <michael.atorres90@gmail.com>
2017-04-28 12:58:40 -0400

    users can now view their profile image and posts

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,15 @@
 class UserController < ApplicationController
   before_action :authenticate_user!
   def show
-
+    @user = current_user
+    @posts = @user.posts
+    # @posts = @user.posts.map do |post|
+    #   post.content
+    # end
   end
 
   def index
   end
-
-  @user = User
 
 
 end
